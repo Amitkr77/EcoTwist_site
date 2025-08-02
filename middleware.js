e41ef00800aef0da7563100893f3bfc4a934cc49
Amitkr77 <amitroyk99@gmail.com>
2025-08-02 13:12:39 +0530

    created middleware for protected route

--- middleware.js
+++ middleware.js
@@ -14,3 +14,27 @@
 // export const config = {
 //   matcher: ['/dashboard/:path*'],
 // };
+
+
+// middleware.js
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const token = req.cookies.get('token')?.value;
+
+  const protectedRoutes = ['/cart', '/checkout', '/account'];
+
+  const isProtected = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route)
+  );
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/cart/:path*', '/checkout/:path*', '/account/:path*'],
+};
