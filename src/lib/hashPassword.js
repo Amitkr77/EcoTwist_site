c379a5e05bc99e1fddc807da294d8f49c6ca8a82
Abhinav Anand <erabhipc@gmail.com>
2025-09-09 16:24:28 +0530

    added compareHashedaPassword function

--- src/lib/hashPassword.js
+++ src/lib/hashPassword.js
@@ -4,3 +4,7 @@ export async function hashPassword(password) {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
 }
+
+export async function comparePassword(password, hashedPassword) {
+  return bcrypt.compare(password, hashedPassword);
+}
