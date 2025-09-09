050ca4a77233b2609a767019b2697ea672d9a8c6
Amitkr77 <amitroyk99@gmail.com>
2025-09-09 18:27:57 +0530

    added sitemap and robot file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
-// next-sitemap.config.js
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'http://localhost:3000', 
-  generateRobotsTxt: true, 
+export default {
+  siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+  generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
   priority: 0.7,
