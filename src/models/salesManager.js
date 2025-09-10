e6f29bc66702cab8124bb9aeff8701e9dbc20c82
Abhinav Anand <erabhipc@gmail.com>
2025-09-10 17:42:33 +0530

    changed default role

--- src/models/salesManager.js
+++ src/models/salesManager.js
@@ -4,7 +4,7 @@ const SalesManagerSchema = new mongoose.Schema({
   name: { type: String, required: true }, 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // hashed
-  role: { type: String, default: "sales_manager" },
+  role: { type: String, default: "manager:sales" },
   createdAt: { type: Date, default: Date.now },
 });
 
