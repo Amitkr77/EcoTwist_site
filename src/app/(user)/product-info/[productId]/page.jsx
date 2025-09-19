2a5d91faa5451928deb5604e0e9c529b2099aac0
Amitkr77 <amitroyk99@gmail.com>
2025-09-19 10:50:14 +0530

    fixed the build error

--- src/app/(user)/product-info/[productId]/page.jsx
+++ src/app/(user)/product-info/[productId]/page.jsx
@@ -61,7 +61,7 @@ import {
 import Link from "next/link";
 import { fetchProducts } from "@/store/slices/productSlices";
 import toast from "react-hot-toast";
-import { jwt_decode } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 export default function ProductPage() {
   const dispatch = useDispatch();
@@ -195,7 +195,7 @@ export default function ProductPage() {
 
   function getUserIdFromToken(token) {
     try {
-      const decoded = jwt_decode(token);
+      const decoded = jwtDecode(token);
       return decoded?.userId || decoded?.sub;
     } catch (error) {
       return null;
