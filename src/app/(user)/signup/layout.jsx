59065ed818124f5ffae0442bf6a9fa6a05762044
Abhinav Anand <erabhipc@gmail.com>
2025-10-11 18:13:23 +0530

    added layout in login and signup page

--- src/app/(user)/signup/layout.jsx
+++ src/app/(user)/signup/layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 
-export default function LoginLayout({ children }) {
+export default function SignupLayout({ children }) {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
